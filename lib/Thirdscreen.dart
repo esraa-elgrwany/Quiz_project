49436838a921fb2606810f3e5a2522fb4d2a8b95
esraa_elgrwany <esraaelgrwany@gmail.com>
2023-09-29 20:31:16 +0300

    app init

---
+++ lib/Thirdscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget{
+  static const String routeName="thirdScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+    );
+  }
+}
---
+++ lib/secondscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget{
+ static const String routeName="secondScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
